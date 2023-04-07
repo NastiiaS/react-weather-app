32eafefedcd4b082581361d9861f25dc27ae89da
NastiiaS <nastia.space111@gmail.com>
2023-04-07 18:59:50 +0100

    Added API integration

--- src/body.js
+++ src/body.js
@@ -1,73 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function Body() {
-  return (
-    <div className="body">
-      <body>
-        <div className="weatherbody">
-          <form id="search-form">
-            <div className="form-group fg--search">
-              <input
-                type="text"
-                placeholder="Enter your city"
-                id="search-city-input"
-              />
-              <button type="submit">
-                <i className="fa fa-search"></i>
-              </button>
-            </div>
-          </form>
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      date: "Wednesday 07:00",
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      iconUrl: "http://openweathermap.org/img/wn/04n@2x.png",
+    });
+  }
 
-          <div className="WeatherInfo">
-            <div className="row">
-              <div className="col-6">
-                <h1 id="city">Kyiv</h1>
-                <ul style={{ "list-style": "none" }}>
-                  <li id="dateMon">23 September</li>
-                  <li id="time">17:39</li>
-                  <li id="description"></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-6">
-              <div class="d-flex weather-temperature">
-                <img
-                  src="http://openweathermap.org/img/wn/04n@2x.png"
-                  alt="Clear"
-                  id="icon"
-                  class="float-left"
+  if (weatherData.ready) {
+    return (
+      <div className="body">
+        <body>
+          <div className="weatherbody">
+            <form id="search-form">
+              <div className="form-group fg--search">
+                <input
+                  type="text"
+                  placeholder="Enter your city"
+                  id="search-city-input"
                 />
-                <div class="float-left">
-                  <strong id="temperature">9</strong>
-                  <span class="units"> °C</span>
+                <button type="submit">
+                  <i className="fa fa-search"></i>
+                </button>
+              </div>
+            </form>
+
+            <div className="WeatherInfo">
+              <div className="row">
+                <div className="col-6">
+                  <h1 id="city">{weatherData.city}</h1>
+                  <ul style={{ "list-style": "none" }}>
+                    <li id="dateMon">{weatherData.date}</li>
+                    <li id="time">17:39</li>
+                    <li id="description" className="text-capitalize">
+                      {weatherData.description}
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
-            <div class="col-3">
-              <ul style={{ "list-style": "none" }}>
-                <li>
-                  Humidity: <span id="humidity">20</span>%
-                </li>
-                <li>
-                  Wind: <span id="wind">9</span> m/h
-                </li>
-              </ul>
+            <div class="row">
+              <div class="col-6">
+                <div class="d-flex weather-temperature">
+                  <img
+                    src={weatherData.iconUrl}
+                    alt={weatherData.description}
+                    id="icon"
+                    class="float-left"
+                  />
+                  <div class="float-left">
+                    <strong id="temperature">
+                      {Math.round(weatherData.temperature)}
+                    </strong>
+                    <span class="units"> °C</span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-3">
+                <ul style={{ "list-style": "none" }}>
+                  <li>
+                    Humidity: <span id="humidity">20</span>%
+                  </li>
+                  <li>
+                    Wind: <span id="wind">{weatherData.wind}</span> m/h
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-          <div class="weather-forecast" id="forecast"></div>
+            <div class="weather-forecast" id="forecast"></div>
 
-          <small id="open-source">
-            <a href="https://github.com/NastiiaS/new-project">
-              Open-source code,
-            </a>
-            by Anastasiia Salenko
-          </small>
-        </div>
-      </body>
-    </div>
-  );
+            <small id="open-source">
+              <a href="https://github.com/NastiiaS/new-project">
+                Open-source code,
+              </a>
+              by Anastasiia Salenko
+            </small>
+          </div>
+        </body>
+      </div>
+    );
+  } else {
+    const apiKey = "daf11acdf17cf256d0449be959aa2959";
+    let city = "London";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
 
 export default Body;
